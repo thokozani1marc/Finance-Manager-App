f781a5f1e5e48a377a74436682895f0ecdf1e730
thokozani1marcs <thokozani1marcus@gmail.com>
2025-08-29 14:48:27 +0200

    added customer invoice and edit form

---
+++ Finance Manager App/CustomerInvoiceEditForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Finance_Manager_App
+{
+    partial class CustomerInvoiceEditForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "CustomerInvoiceEditForm";
+
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCustomer = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInvoiceNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer:";
+            // 
+            // cmbCustomer
+            // 
+            this.cmbCustomer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCustomer.FormattingEnabled = true;
+            this.cmbCustomer.Location = new System.Drawing.Point(120, 12);
+            this.cmbCustomer.Name = "cmbCustomer";
+            this.cmbCustomer.Size = new System.Drawing.Size(200, 21);
+            this.cmbCustomer.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Invoice Number:";
+            // 
+            // txtInvoiceNumber
+            // 
+            this.txtInvoiceNumber.Location = new System.Drawing.Point(120, 39);
+            this.txtInvoiceNumber.Name = "txtInvoiceNumber";
+            this.txtInvoiceNumber.Size = new System.Drawing.Size(200, 20);
+            this.txtInvoiceNumber.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date:";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(120, 65);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpDate.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 94);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Amount:";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(120, 91);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(200, 20);
+            this.txtAmount.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Due Date:";
+            // 
+            // dtpDueDate
+            // 
+            this.dtpDueDate.Location = new System.Drawing.Point(120, 117);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpDueDate.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Status:";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Items.AddRange(new object[] {
+            "Pending",
+            "Paid",
+            "Overdue"});
+            this.cmbStatus.Location = new System.Drawing.Point(120, 143);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(200, 21);
+            this.cmbStatus.TabIndex = 11;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(164, 170);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(245, 170);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // CustomerInvoiceEditForm
+            // 
+            this.AcceptButton = this.btnSave;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(332, 205);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtpDueDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtInvoiceNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbCustomer);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerInvoiceEditForm";
+            this.Text = "Edit Customer Invoice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ Finance Manager App/CustomerInvoiceEditForm.cs
@@ -0,0 +1,102 @@
+﻿using Finance_Manager_App.Models;
+using Org.BouncyCastle.Asn1.Cmp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_Manager_App
+{
+    public partial class CustomerInvoiceEditForm : Form
+    {
+        private CustomerInvoice _invoice;
+        private List<Customer> _customers;
+        public CustomerInvoiceEditForm()
+        {
+            InitializeComponent();
+            _invoice = new CustomerInvoice();
+            LoadCustomers();
+        }
+
+        public CustomerInvoiceEditForm(CustomerInvoice invoice)
+        {
+            InitializeComponent();
+            _invoice = invoice;
+            LoadCustomers();
+
+            // Set form values from the invoice
+            cmbCustomer.SelectedValue = invoice.CustomerId;
+            txtInvoiceNumber.Text = invoice.InvoiceNumber;
+            dtpDate.Value = invoice.Date;
+            txtAmount.Text = invoice.Amount.ToString();
+            dtpDueDate.Value = invoice.DueDate;
+            cmbStatus.Text = invoice.Status;
+        }
+
+        private void LoadCustomers()
+        {
+            _customers = DataAccess.GetCustomers();
+            cmbCustomer.DataSource = _customers;
+            cmbCustomer.DisplayMember = "Name";
+            cmbCustomer.ValueMember = "CustomerId";
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtInvoiceNumber.Text))
+            {
+                MessageBox.Show("Please enter an invoice number");
+                return;
+            }
+
+            if (!decimal.TryParse(txtAmount.Text, out decimal amount))
+            {
+                MessageBox.Show("Please enter a valid amount");
+                return;
+            }
+
+            _invoice.CustomerId = (int)cmbCustomer.SelectedValue;
+            _invoice.InvoiceNumber = txtInvoiceNumber.Text;
+            _invoice.Date = dtpDate.Value;
+            _invoice.Amount = amount;
+            _invoice.DueDate = dtpDueDate.Value;
+            _invoice.Status = cmbStatus.Text;
+
+            bool success;
+            if (_invoice.InvoiceId == 0)
+                success = DataAccess.AddCustomerInvoice(_invoice);
+            else
+                success = false; // Update functionality to be implemented
+
+            if (success)
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Error saving customer invoice");
+            }
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCustomer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInvoiceNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
