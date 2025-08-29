abf7ae0ac98124c7a79c636c025d70d02389f8fe
thokozani1marcs <thokozani1marcus@gmail.com>
2025-08-29 14:37:40 +0200

    Added Expense form and edit

---
+++ Finance Manager App/ExpenseEditForm.cs
@@ -0,0 +1,82 @@
+﻿using Finance_Manager_App.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_Manager_App
+{
+    public partial class ExpenseEditForm : Form
+    {
+        private Expense _expense;
+        public ExpenseEditForm()
+        {
+            InitializeComponent();
+            _expense = new Expense();
+        }
+
+        public ExpenseEditForm(Expense expense)
+        {
+            InitializeComponent();
+            _expense = expense;
+
+            // Set form values from the expense
+            txtCategory.Text = expense.Category;
+            dtpDate.Value = expense.Date;
+            txtAmount.Text = expense.Amount.ToString();
+            txtDescription.Text = expense.Description;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCategory.Text))
+            {
+                MessageBox.Show("Please enter a category");
+                return;
+            }
+
+            if (!decimal.TryParse(txtAmount.Text, out decimal amount))
+            {
+                MessageBox.Show("Please enter a valid amount");
+                return;
+            }
+
+            _expense.Category = txtCategory.Text;
+            _expense.Date = dtpDate.Value;
+            _expense.Amount = amount;
+            _expense.Description = txtDescription.Text;
+
+            bool success;
+            if (_expense.ExpenseId == 0)
+                success = DataAccess.AddExpense(_expense);
+            else
+                success = false; // Update functionality to be implemented
+
+            if (success)
+            {
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Error saving expense");
+            }
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ Finance Manager App/ExpensesForm.cs
@@ -0,0 +1,70 @@
+﻿using Finance_Manager_App.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Finance_Manager_App
+{
+    public partial class ExpensesForm : Form
+    {
+        public ExpensesForm()
+        {
+            InitializeComponent();
+            LoadExpenses();
+        }
+
+
+        private void LoadExpenses()
+        {
+            List<Expense> expenses = DataAccess.GetExpenses();
+            dataGridViewExpenses.DataSource = expenses;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var form = new ExpenseEditForm();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                LoadExpenses();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewExpenses.CurrentRow != null)
+            {
+                Expense expense = (Expense)dataGridViewExpenses.CurrentRow.DataBoundItem;
+                var form = new ExpenseEditForm(expense);
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    LoadExpenses();
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewExpenses.CurrentRow != null)
+            {
+                Expense expense = (Expense)dataGridViewExpenses.CurrentRow.DataBoundItem;
+                if (MessageBox.Show($"Delete expense {expense.Category} - {expense.Description}?", "Confirm",
+                    MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    // Implement delete functionality
+                    MessageBox.Show("Delete functionality to be implemented");
+                }
+            }
+        }
+
+        private System.Windows.Forms.DataGridView dataGridViewExpenses;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
